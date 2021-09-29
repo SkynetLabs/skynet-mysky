117a442c2166688b2f7e107ed3b6d100d120c2e5
Marcin S <marcin@bytedude.com>
2021-09-29 18:39:37 -0500

    Address review comment

--- utilities/recover-phrase-from-seed.ts
+++ utilities/recover-phrase-from-seed.ts
@@ -1,9 +1,22 @@
 /**
+ * Utility for recovering the original phrase if only the seed is known.
+ *
  * Example usage:
+ *
  * ts-node ./utilities/recover-phrase-from-seed.ts "[240, 156, 64,  52, 23, 219, 229,  35, 54, 134, 90,  35, 116,  25, 77,  55]"
  */
 
 import { seedToPhrase } from "../src/seed";
+
+if (process.argv.length <= 2) {
+  console.log(`Utility for recovering the original phrase if only the seed is known.
+
+Example usage:
+
+ts-node ./utilities/recover-phrase-from-seed.ts "[240, 156, 64,  52, 23, 219, 229,  35, 54, 134, 90,  35, 116,  25, 77,  55]`);
+
+  process.exit();
+}
 
 // Transform every file passed in to the script.
 process.argv.forEach(function (arg, index) {
