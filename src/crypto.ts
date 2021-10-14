0c3eeff10e644a6df6d252d6706b331ab0f6768e
Marcin S <marcin@bytedude.com>
2021-10-14 18:23:29 -0500

    Add `deriveEncryptedPathSeedForRoot` for deriving the root path seed
    See comment on https://github.com/SkynetLabs/skynet-js/pull/306 for why we
    are not simply using `deriveEncryptedPathSeed`.
    
    Note: These changes should be totally backward-compatible to avoid loss of data.
    Please see integration tests added in
    https://github.com/SkynetLabs/test-skapp/pull/4.

---
+++ src/crypto.ts
@@ -0,0 +1,34 @@
+import { hash, sign } from "tweetnacl";
+
+import { KeyPair, stringToUint8ArrayUtf8 } from "skynet-js";
+import { toHexString } from "skynet-js/dist/cjs/utils/string";
+
+const SALT_ROOT_DISCOVERABLE_KEY = "root discoverable key";
+
+/**
+ * Generates a keypair from the given user seed. It first salts the seed.
+ *
+ * @param seed - The user seed as bytes.
+ * @returns - The keypair.
+ */
+export function genKeyPairFromSeed(seed: Uint8Array): KeyPair {
+  const bytes = new Uint8Array([...sha512(SALT_ROOT_DISCOVERABLE_KEY), ...sha512(seed)]);
+  const hashBytes = sha512(bytes).slice(0, 32);
+
+  const { publicKey, secretKey } = sign.keyPair.fromSeed(hashBytes);
+
+  return { publicKey: toHexString(publicKey), privateKey: toHexString(secretKey) };
+}
+
+/**
+ * Hashes the given string or byte array using sha512.
+ *
+ * @param message - The string or byte array to hash.
+ * @returns - The resulting hash.
+ */
+export function sha512(message: Uint8Array | string): Uint8Array {
+  if (typeof message === "string") {
+    return hash(stringToUint8ArrayUtf8(message));
+  }
+  return hash(message);
+}
