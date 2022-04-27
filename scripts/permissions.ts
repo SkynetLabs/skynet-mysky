8737d41cbbfc2b24c205e867704fd85af74baa99
Marcin S <marcin@bytedude.com>
2022-04-27 16:44:58 -0500

    Fix permissions test

--- scripts/permissions.ts
+++ scripts/permissions.ts
@@ -7,6 +7,8 @@ import { ChildHandshake, WorkerMessenger } from "post-me";
 import type { Connection } from "post-me";
 import { CheckPermissionsResponse, getParentPath, getPathDomain, Permission, sanitizePath } from "skynet-mysky-utils";
 
+import { createPermissionBitfield, createPermissionKey } from "../src/permissions";
+
 const version = 2;
 const versionKey = "_v";
 
@@ -184,41 +186,4 @@ async function savePermission(perm: Permission): Promise<void> {
   const key = createPermissionKey(perm.requestor, perm.path);
   const bitfieldToAdd = createPermissionBitfield(perm.category, perm.permType);
   await update(key, (storedBitfield: number | undefined) => (storedBitfield || 0) | bitfieldToAdd);
-}
-
-// =======
-// Helpers
-// =======
-
-/**
- * Creates the bitfield for the given category and permission type.
- *
- * @param category - The permission category.
- * @param permType = The permission type.
- * @returns - The bitfield.
- */
-function createPermissionBitfield(category: number, permType: number): number {
-  // Reserve space for 16 perm types.
-  const bit = (category - 1) * 16 + permType;
-  return 1 << bit;
-}
-
-/**
- * Creates the permission key for the given requestor and path.
- *
- * @param requestor - The permission requestor.
- * @param path - The permission path.
- * @returns - The permission key.
- * @throws - Will throw if the requestor or path are invalid.
- */
-export function createPermissionKey(requestor: string, path: string): string {
-  const sanitizedRequestor = sanitizePath(requestor);
-  if (sanitizedRequestor === null) {
-    throw new Error(`Invalid requestor: '${requestor}'`);
-  }
-  const sanitizedPath = sanitizePath(path);
-  if (sanitizedPath === null) {
-    throw new Error(`Invalid path: '${path}'`);
-  }
-  return `[${sanitizedRequestor}],[${sanitizedPath}]`;
 }
